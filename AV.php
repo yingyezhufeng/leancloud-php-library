6f85a32f179f2fb71108cc39a706801740db44d9
dennis zhuang <killme2008@gmail.com>
2014-01-18 18:11:38 +0800

    Remove printStackTrace

--- AV.php
+++ AV.php
@@ -182,29 +182,6 @@ class AVRestClient{
 		throw new AVLibraryException($msg,$code);
 	}
 
-
-	function printStackTrace() {
-    $stack = debug_backtrace();
-    $output = 'Stack trace:' . PHP_EOL;
-
-    $stackLen = count($stack);
-    for ($i = 1; $i < $stackLen; $i++) {
-        $entry = $stack[$i];
-
-        $func = $entry['function'] . '(';
-        $argsLen = count($entry['args']);
-        for ($j = 0; $j < $argsLen; $j++) {
-            $func .= $entry['args'][$j];
-            if ($j < $argsLen - 1) $func .= ', ';
-        }
-        $func .= ')';
-
-        $output .= '#' . ($i - 1) . ' ' . $entry['file'] . ':' . $entry['line'] . ' - ' . $func . PHP_EOL;
-    }
-
-	print_r($output);
-}
-
 	private function checkResponse($response,$responseCode,$expectedCode){
 		//TODO: Need to also check for response for a correct result from AVOSCloud.com
 		if(!in_array($responseCode,$expectedCode)){
