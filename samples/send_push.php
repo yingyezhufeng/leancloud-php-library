6552c7638277f6b87ca1642ff578e95bbe2317d6
dennis zhuang <killme2008@gmail.com>
2014-01-21 21:10:13 +0800

    Format samples

--- samples/send_push.php
+++ samples/send_push.php
@@ -1,8 +1,8 @@
 <?php
-    include_once 'AV.php';
-	$push =new AVPush;
-	$push->alert = 'Hello from AVOS Cloud';
-	$push->channels = array('foo', 'bar');
-	$return = $push->send();
-    print_r($return);
+include_once 'AV.php';
+$push =new AVPush;
+$push->alert = 'Hello from AVOS Cloud';
+$push->channels = array('foo', 'bar');
+$return = $push->send();
+print_r($return);
 ?>
--- samples/upload_file.php
+++ samples/upload_file.php
@@ -1,10 +1,10 @@
 <?php
-	include_once 'AV.php';
-	$file = new AVFile('text/plain', 'Working at AVOS Cloud is Great');
-	$save = $file->save('hello.txt');
-	print_r($save);
+include_once 'AV.php';
+$file = new AVFile('text/plain', 'Working at AVOS Cloud is Great');
+$save = $file->save('hello.txt');
+print_r($save);
 
-    $obj = new AVObject('GameScore');
-    $obj->image = $obj->dataType('file', $save->objectId);
-    $obj->save();
+$obj = new AVObject('GameScore');
+$obj->image = $obj->dataType('file', $save->objectId);
+$obj->save();
 ?>
