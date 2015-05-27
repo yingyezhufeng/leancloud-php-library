7e824cd7f4ebdd4c4020b2d67097e9b2c629ca32
yingyezhufeng <1058554775@qq.com>
2015-05-27 12:02:17 +0800

    添加配置文件

---
+++ AVConfig.php
@@ -0,0 +1,11 @@
+<?php namespace leancloud;
+
+class AVConfig{
+
+    const APPID = 'ay476i55kmtitzgcqmfzk1fj1cdkk87sc0d7n5opwqszf8sg';
+    const MASTERKEY = 'gih1p7ie3oo3oy8jjnp100miej9nzzfbovr96kld0milo4dk';
+    const APPKEY = 'cgwy0xfnz1mxbb2wunp1k512bretcq0e2pswxtgx03f6q7vk';
+    const AVOSCLOUDURL = 'https://leancloud.cn/1.1/';
+}
+
+?>
